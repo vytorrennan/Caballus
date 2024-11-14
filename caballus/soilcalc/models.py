5142b05bfd6b07435eda84fd41c0e0536abb81b6
Rian <riangabrieldev@gmail.com>
2024-11-14 09:05:05 -0300

    creating the soil sample model

--- caballus/soilcalc/models.py
+++ caballus/soilcalc/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class sampleSoil(models.Model):
+    sample = models.IntergerField(max=200)
+    ph = models.FloatField(blank=True)
+    organicMaterial = models.FloatField(blank=True)
+    phosporo = models.FLoatField(blank=True)
+    potassium = models.FloatField(blank=True)
+    na = models.FloatField(blank=True)
+    ca = models.FloatField(blank=True)
+    mg = models.FloatField(blank=True)
+    al = models.FloatField(blank=True)
+    hAl = models.FloatField(blank=True)
+    cu = models.FloatField(blank=True)
+    fe = models.FloatField(blank=True)
+    mn = models.FloatField(blank=True)
+    zn = models.FloatField(blank=True)
+    pRem = models.FloatField(blank=True)
+    emptyBecker1 = models.FloatField(blank=True)
+    emptyBecker2 = models.FloatField(blank=True)
+    claySilt = models.FloatField(blank=True)
+    clay = models.FloatField(blank=True)
