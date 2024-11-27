710b1d9880b352730bf64efea919ffad06b930e1
Rian <riangabrieldev@gmail.com>
2024-11-26 21:35:56 -0300

    Add viws and urls for calculatios, anda templates for tests

--- caballus/soilcalc/views.py
+++ caballus/soilcalc/views.py
@@ -1,3 +1,27 @@
+from os import walk
+from django.http import request
 from django.shortcuts import render
+from soilcalc.services import ph_calculations
+from .models import sampleSoil
 
 # Create your views here.
+
+
+def view_sample(request):
+    sample = None
+    sample_id = None
+    if request.method == 'POST':
+        sample_id = request.POST.get('sample_id')
+        if sample_id:
+            try:
+                sample = sampleSoil.objects.get(sample=sample_id)
+            except: 
+                sample = None
+    #ph_calculations(sample.)
+
+    context = {
+        'sample_id': sample_id, 
+        "sample": sample.__dict__ if sample else None }
+
+    return render(request, "soilcalc/index.html", context)
+
