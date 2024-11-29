feb179de7b5aeb59fc7750f7425a2841199cdfb9
Rian <riangabrieldev@gmail.com>
2024-11-29 09:36:24 -0300

    fix: fix error for query in the new database model

--- caballus/soilcalc/views.py
+++ caballus/soilcalc/views.py
@@ -15,13 +15,18 @@ def view_sample(request):
         sample_id = request.POST.get('sample_id')
         if sample_id:
             try:
+
                 sample = sampleSoil.objects.get(sample=sample_id)
                 ph = ph_calculations.calculate_soil_ph(sample.ca, sample.mg)
+                sample = sample.__dict__ #transforma o objeto em um dicionario
+                sample.pop(next(iter(sample)))
+
             except: 
                 sample = None
+
     context = {
         'sample_id': sample_id,
-        'sample': sample.__dict__ if sample else None,
+        'sample': sample,
         'ph': ph
     }
 
