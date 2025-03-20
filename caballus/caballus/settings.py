47b8801c63b711450b29d029d301189dc1c62b16
PedroG022 <55058149+PedroG022@users.noreply.github.com>
2025-03-20 15:32:09 -0300

    fix: Fix missing comma in settings.py

--- caballus/caballus/settings.py
+++ caballus/caballus/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dataimport'
+    'dataimport',
     'loginSystem',
     'soilcalc',
 ]
