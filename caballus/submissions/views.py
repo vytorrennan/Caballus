e89283e3f0cbf5c49594b911e922375f745517db
Rian <riangabrieldev@gmail.com>
2025-04-24 15:12:07 +0000

    add: main confgs for submissions

--- caballus/submissions/views.py
+++ caballus/submissions/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+class Submit(View):
+    def get(self, request):
+        return render(request, 'index.html')
