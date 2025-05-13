20720b70d2683d667d196831a052634a658d64ef
Rian <riangabrieldev@gmail.com>
2025-05-13 17:26:05 +0000

    rebase: templates

--- caballus/submissions/urls.py
+++ caballus/submissions/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Submit.as_view(), name="Submit"),
+    path("", views.Submit.as_view(), name="submit"),
 ]
--- caballus/submissions/views.py
+++ caballus/submissions/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render, redirect
 
 class Submit(View):
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, 'submissions.html')
+
+    def post(self, request):
+        return render(request, 'submissions.html')
