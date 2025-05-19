1e283b28d5ed6e4b02bb0337caf8730e25424af0
Rian <riangabrieldev@gmail.com>
2025-05-19 17:41:30 +0000

    add: bd for sunmissions

--- caballus/submissions/models.py
+++ caballus/submissions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class sunmission_sample(models.Model):
+    id = models.IntegerField(primary_key=True, default=None) 
+    created_at = models.DateField(default=now) 
+    content = models.CharField(max_length=150)
+   
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='submissions'
+    )
+
