d43e0ba8c748c0d7866429cbeea3de3e1fc91b07
Rian <riangabrieldev@gmail.com>
2025-06-10 21:44:12 +0000

    rebase: bd for submissions, form for submissions, add forms inputs in the bd

--- caballus/caballus/settings.py
+++ caballus/caballus/settings.py
@@ -13,12 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import environ
 
-env = environ.Env()
-environ.Env.read_env()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+# Lê explicitamente o .env que fica no diretório do projeto
+env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
--- caballus/submissions/admin.py
+++ caballus/submissions/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import (submissionSample)
+from .models import (SubmissionSample)
 
 
-admin.site.register(submissionSample)
+admin.site.register(SubmissionSample)
 
 
